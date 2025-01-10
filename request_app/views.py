fed52890fa68a8b4b7fc365c947dfc2b171403df
Andreeva Yulia <Fantosmagoria1994@yandex.ru>
2025-01-10 16:24:35 +0300

    another file

--- request_app/views.py
+++ request_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.template.context_processors import request
 
 
-MAX_FILE_SIZE= 10 * 1024 # 10 Kb
+MAX_FILE_SIZE= 1 * 1024 * 2 # 1 Mb
 
 def process_get(request: HttpRequest) -> HttpResponse:
     a = request.GET.get('a', '')
@@ -26,15 +26,13 @@ def upload_form(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST' and request.FILES.get('myfile'):
         myfile = request.FILES['myfile']
         if myfile.size > MAX_FILE_SIZE:
-            print(f'Файл слишком большой! Максимальный размер {MAX_FILE_SIZE // 1024}KB')
+            print(f'Файл слишком большой! Максимальный размер {MAX_FILE_SIZE // 1024 * 2}MB')
             return render(request, 'request_app/file_upload.html')
 
         fs = FileSystemStorage()
         try:
             filename = fs.save(myfile.name, myfile)
             print(f'сохранили файл: {filename}')
-            return render(request, 'request_app/file_upload.html')
         except Exception as e:
             print(f'Ошибка загрузки файла {e}')
-            return render(request, 'request_app/file_upload.html')
     return render(request, 'request_app/file_upload.html')
